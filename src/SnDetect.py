afabadb5917f8b16f6d03e2938ba7eb00e995713
skunx <freebyte@free.fr>
2012-01-22 19:59:01 +0100

    Adding sources

---
+++ src/SnDetect.py
@@ -0,0 +1,153 @@
+#! /usr/bin/env python
+'''
+@author: Daouzli
+'''
+import SnGui      #container for ip->account->contact
+import SnInterf   #gtk window with the treeview
+import SnProtocol #filtering packets with msn protocol
+
+import re
+import sys
+import pcap
+import string
+import time
+import socket
+import struct
+import gobject
+
+global g    #class SnGui 
+global tvSn #class SnInterf
+global prot #class SnProtocol
+    
+protocols={socket.IPPROTO_TCP:'tcp',
+            socket.IPPROTO_UDP:'udp',
+            socket.IPPROTO_ICMP:'icmp'}
+
+def decode_ip_packet(s):
+    d={}
+    d['version']=(ord(s[0]) & 0xf0) >> 4
+    d['header_len']=ord(s[0]) & 0x0f
+    d['tos']=ord(s[1])
+    d['total_len']=socket.ntohs(struct.unpack('H',s[2:4])[0])
+    d['id']=socket.ntohs(struct.unpack('H',s[4:6])[0])
+    d['flags']=(ord(s[6]) & 0xe0) >> 5
+    d['fragment_offset']=socket.ntohs(struct.unpack('H',s[6:8])[0] & 0x1f)
+    d['ttl']=ord(s[8])
+    d['protocol']=ord(s[9])
+    d['checksum']=socket.ntohs(struct.unpack('H',s[10:12])[0])
+    d['source_address']=pcap.ntoa(struct.unpack('i',s[12:16])[0])
+    d['destination_address']=pcap.ntoa(struct.unpack('i',s[16:20])[0])
+    if d['header_len']>5:
+        d['options']=s[20:4*(d['header_len']-5)]
+    else:
+        d['options']=None
+    d['data']=s[4*d['header_len']:]
+    return d
+ips={}
+
+
+def det_IP_packet(addr):
+    """det_IP_packet(addr): add the IP addr to the stored data and add it to the treeview"""
+    global g,tvSn
+    global ips
+    if addr not in ips.keys():
+        ips[addr]={}
+        g.add_ip(addr)
+        #tvSn.Create_Sn_Tree()
+        #tvSn.Update_Sn_Tree()
+        tvSn.AddIfNot_Sn_ip_Tree(addr)
+   
+def det_elems_data(data,src,dst):
+    """det_elems_data(data,src,dst): analyse the data from packet sent from src IP to dst IP"""
+    global g,prot,tvSn
+    print len(data)
+    cmd=re.findall(r'^(\w+)\s',data)
+    if len(cmd)>0:
+        for i in cmd:
+            print "COMMAND : "+i
+
+    prot.set_data(data)
+    if prot.is_email()==True: #if the data contains an email
+        mail=prot.get_email()
+        for i in mail:
+            print i+" from "+src+" to "+dst
+    adrip=dst if src[:6]=='207.46' or src[:4]=='65.5' else src #if M$ server then use the other ip adress
+    
+    if prot.is_typing()==True: #if we have a packet saying that someone is typing a message
+        adr=prot.get_typing()
+        print "typing:"+adr
+        if g.is_ip(adrip)==False:
+            g.add_ip(adrip)
+        if g.ip(adrip).is_account(adr.strip())==False:
+            g.ip(adrip).add_account(adr.strip())
+            tvSn.AddIfNot_Sn_account_Tree(adrip,adr.strip())
+        #tvSn.Create_Sn_Tree()
+        #tvSn.Update_Sn_Tree()
+        #print ">>"+adrip+"->"+
+        #g.ip(adrip).account(adr.strip()).add_to_speech("[scribing]")
+                    
+    if prot.is_msg()==True: #If we have a chat message
+        if prot.is_msg_received():
+            mail=prot.get_email_msg_sender()
+            msg=prot.get_msg()
+            if g.is_ip(adrip)==False: #does this IP already exists in the SnGui
+                g.add_ip(adrip)
+            if g.ip(adrip).account_id(0).is_contact(mail)==False: #does this contact exists
+                g.ip(adrip).account_id(0).add_contact(mail)
+                tvSn.AddIfNot_Sn_contact_Tree(adrip,g.ip(adrip).account_id(0).account,mail)
+            g.ip(adrip).account_id(0).contact(mail).add_to_speech(msg+'\n') #add the message to the contact
+            tvSn.show()
+            #tvSn.Create_Sn_Tree()
+
+
+def process_packet(pktlen, data, timestamp):
+    if not data:
+        return
+
+    if data[12:14]=='\x08\x00':
+        decoded=decode_ip_packet(data[14:])
+        det_IP_packet(decoded['destination_address'])
+        det_IP_packet(decoded['source_address'])
+        det_elems_data(decoded['data'],decoded['source_address'],decoded['destination_address'])
+
+
+def main(p):
+    #SnInterf.gtk.timeout_add(100,function_tempo,p)#each 100 ms function_tempo is called
+    gobject.timeout_add(100,function_tempo,p)#each 100 ms the packet function dispatch is called
+    gobject.timeout_add(100,function_tempo_refresh)#each 100 ms shows the window with treeview
+    SnInterf.gtk.main()
+
+
+def function_tempo(p):
+    p.dispatch(1, process_packet)
+    return True
+
+
+def function_tempo_refresh():
+    tvSn.show()
+    return True
+
+
+if __name__=="__main__":
+    global g,tvSn
+
+    p = pcap.pcapObject()
+    port_ecoute="port 1863"
+    dev = "eth0"
+    net, mask = pcap.lookupnet(dev)
+    p.open_live(dev, 1600, 0, 100)
+    p.setfilter(port_ecoute, 0, 0)
+    g=SnGui.Sn_Gui() #container for ip->account->contact
+    tvSn=SnInterf.Sn_Gtk_Window(g) #gtk window with the treeview
+    prot=SnProtocol.SnProtocol() #filtering packets with msn protocol
+
+    main(p)
+    
+#    try:
+#        while fin==False:
+#    except KeyboardInterrupt:
+#        print '%s' % sys.exc_type
+#        print 'shutting down'
+#        print '%d packets received, %d packets dropped, %d packets dropped by interface' %  p.stats()
+    
+
---
+++ src/SnInterf/__init__.py
@@ -0,0 +1,221 @@
+#! /usr/bin/env python
+
+'''
+@author: Daouzli
+'''
+
+
+import SnGui #container for ip->account->contact
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+
+class Sn_Gtk_Window():
+    """Class containing the GtkWindow and the treeview"""
+    # close the window and quit
+    def delete_event(self, widget, event, data=None):
+        gtk.main_quit()
+        return False
+
+    def __init__(self,sn):
+        self.sn=sn
+        # Create a new window
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title("SnMSN Gui")
+        self.window.set_size_request(600, 200)
+        self.window.connect("delete_event", self.delete_event)
+        # create a TreeStore with one string column to use as the model
+        self.explorer_tree = gtk.TreeStore(str)
+        # create the TreeView using explorer_tree
+        self.treeview = gtk.TreeView(self.explorer_tree)
+        treeselection = self.treeview.get_selection()
+        treeselection.set_mode(gtk.SELECTION_SINGLE)
+        self.treeview.connect("row-activated", self.Selection)  
+        # create the TreeViewColumn to display the data
+        self.tvcolumn = gtk.TreeViewColumn('Explorer')
+        # add tvcolumn to treeview
+        self.treeview.append_column(self.tvcolumn)
+        # create a CellRendererText to render the data
+        self.cell = gtk.CellRendererText()
+        # add the cell to the tvcolumn and allow it to expand
+        self.tvcolumn.pack_start(self.cell, True)
+        # set the cell "text" attribute to column 0 - retrieve text
+        # from that column in explorer_tree
+        self.tvcolumn.add_attribute(self.cell, 'text', 0)
+        # make it searchable
+        self.treeview.set_search_column(0)
+        #scrolltreeview = gtk.VScrollbar(adjustment=None)
+        #scrolltreeview.add_child(self.treeview)
+        #create the data window
+        self.data_window = gtk.Notebook()
+        #create the text zone for the speech
+        self.label_speech = gtk.Label("[No speech]")
+        
+        self.gui = gtk.HBox(False, 2)
+        self.data_win = gtk.VBox(False, 1)
+        
+        self.label_select=gtk.Label("[No selection]")
+        self.label_select.set_alignment(0, 0)
+        
+        self.data_win.pack_start(self.label_select,True,True,0)
+        self.data_win.pack_start(self.data_window,True,True,0)
+        
+        #treeview is placed on the left, then its vscrollbar, and then the data window on the right
+        self.tvscrv=gtk.VScrollbar(self.treeview.get_vadjustment())
+        self.gui.pack_start(self.treeview,True,True,10)
+        self.gui.pack_start(self.tvscrv,True,True,10)
+        self.gui.pack_start(self.data_win,True,True,10)
+        
+        #config the data window zone
+        self.Config_Data_Window()
+        
+        self.window.add(self.gui)
+
+        self.window.show_all()
+
+    def Selection(self, widget, event, data=None):
+        """Selection(): shows the selected contact item in data window label zone, 
+                        and if there is a speech, shows it on the speech zone.
+                        if the selection is not a contact, it expands or collapses the child rows """
+        if len(event)==3: #work only in the 3rd level (selection of a contact)
+            ip,acc,cont=event
+            s='%s->%s->%s' %  (self.sn.ip_id(ip).ip,
+                                     self.sn.ip_id(ip).account_id(acc).account,
+                                     self.sn.ip_id(ip).account_id(acc).contact_id(cont).contact)
+            self.set_label_selected(s)
+
+            self.set_label_speech(self.sn.ip_id(ip).account_id(acc).contact_id(cont).speech)
+        else:
+            if self.treeview.row_expanded(event):
+                self.treeview.collapse_row(event)
+            else:
+                self.treeview.expand_row(event,False)
+    
+    def Config_Data_Window(self):
+        """Config_Data_Window(): creates the data window zone (right part)"""
+        self.data_window.set_tab_pos(2) #the tabs will be placed on the top
+        label=gtk.Label("Speech")#the first tab's label is Speech
+        self.data_window.append_page(self.label_speech, label)#add the speech zone to the data window notebook
+        self.window.show_all()
+        
+    def set_label_selected(self,label):
+        """Set_label_Speech(speech): Sets the text of the speech showed on the data window speech tab"""
+        self.label_select.set_text(label)
+        self.window.show_all()
+        
+    def set_label_speech(self,speech):
+        """Set_label_Speech(speech): Sets the text of the speech showed on the data window speech tab"""
+        self.label_speech.set_text(speech)
+        self.window.show_all()
+        
+    def Create_Sn_Tree(self):
+        """Create_Sn_Tree(sn): creates (after erasing old) and shows a treeview based on the Sn_Gui object content"""
+        sn=self.sn
+        self.explorer_tree.clear()
+        for ip in sn.list_ips():
+            parent_ip=self.explorer_tree.append(None, ['%s' % sn.ip(ip).ip])
+            print "i"+str(parent_ip)
+            for acc in sn.ip(ip).list_accounts():
+                child_acc=self.explorer_tree.append(parent_ip, ['%s' % sn.ip(ip).account(acc).account])
+                print "  a"+str(child_acc)
+                for cont in sn.ip(ip).account(acc).list_contacts():
+                    print "     "+str(self.explorer_tree.append(child_acc, ['%s' % sn.ip(ip).account(acc).contact(cont).contact]))
+        self.window.show_all()
+    
+    def Update_Sn_Tree(self):
+        """Update_Sn_Tree(): Searchs in Sn object if there are data not yet in the tree and update it if necessary"""
+        sn=self.sn
+        tr=self.explorer_tree
+        for ip in sn.list_ips():
+            self.AddIfNot_Sn_ip_Tree(ip)
+            for acc in sn.ip(ip).list_accounts():
+                self.AddIfNot_Sn_account_Tree(ip,acc)
+                for cont in sn.ip(ip).account(acc).list_contacts():
+                    self.AddIfNot_Sn_contact_Tree(ip,acc,cont)
+        
+    def show(self):
+        self.window.show_all()
+        
+    def AddIfNot_Sn_ip_Tree(self,ip):
+        """AddIfNot_Sn_ip_Tree(ip): add the ip to the tree if doesn't exists and return the iter"""
+        tr=self.explorer_tree
+        nb=tr.iter_n_children(None)
+        is_ip=False
+        for i in range(nb):
+            ipiter=tr.iter_nth_child(None,i)
+            if tr.get_value(ipiter,0)==ip:
+                is_ip=True
+                break
+        if is_ip==False:
+            ipiter=self.explorer_tree.append(None, ['%s' % ip])
+        return ipiter
+            
+    def AddIfNot_Sn_account_Tree(self,ip,acc):
+        """AddIfNot_Sn_account_Tree(ip,acc): add the ip->acc to the tree if doesn't exists and return the iter"""
+        ipiter=self.AddIfNot_Sn_ip_Tree(ip)
+        tr=self.explorer_tree
+        is_acc=False
+        nb=tr.iter_n_children(ipiter)
+        for i in range(nb):
+            acciter=tr.iter_nth_child(ipiter,i)
+            if tr.get_value(acciter,0)==acc:
+                is_acc=True 
+                break  
+        if is_acc==False:
+            acciter=self.explorer_tree.append(ipiter, ['%s' % acc])
+        return acciter
+
+    def AddIfNot_Sn_contact_Tree(self,ip,acc,cont):
+        """AddIfNot_Sn_contact_Tree(ip,acc,cont): add the ip->acc->cont to the tree if doesn't exists and return the iter"""
+        acciter=self.AddIfNot_Sn_account_Tree(ip,acc)
+        tr=self.explorer_tree
+        is_cont=False
+        nb=tr.iter_n_children(acciter)
+        for i in range(nb):
+            contiter=tr.iter_nth_child(acciter,i)
+            if tr.get_value(contiter,0)==cont:
+                is_cont=True 
+                break  
+        if is_cont==False:
+            contiter=self.explorer_tree.append(acciter, ['%s' % cont])
+        return contiter
+
+if __name__ == "__main__":
+
+    def main():
+        gtk.main()
+
+    g=SnGui.Sn_Gui()
+    tvSn=Sn_Gtk_Window(g)
+
+    g.add_ip("192.168.0.10")
+    g.ip("192.168.0.10").add_account("popo@hotmail.com")
+    g.ip("192.168.0.10").add_account("didi@hotmail.com")
+    g.ip("192.168.0.10").account("didi@hotmail.com").add_contact("coco")
+    g.ip("192.168.0.10").account("popo@hotmail.com").add_contact("coco")
+    g.ip("192.168.0.10").account("popo@hotmail.com").contact("coco").new_speech("je suis le coco de popo")
+    g.ip("192.168.0.10").account("popo@hotmail.com").add_contact("titi")
+
+    g.add_ip("192.168.0.3")
+    g.ip("192.168.0.3").add_account("piouf@hotmail.com")
+    g.ip("192.168.0.3").add_account("raaaa@hotmail.com")
+    g.ip("192.168.0.3").account("piouf@hotmail.com").add_contact("nun")
+    g.ip("192.168.0.3").account("piouf@hotmail.com").contact("nun").new_speech("moi le nun de piouf")
+    g.ip("192.168.0.3").account("piouf@hotmail.com").contact("nun").add_to_speech("\nah bon!\noui oui tout a fit!\n\nhihi hoho que c'est trop rigolo poils au dos")
+    
+
+    g.add_ip("192.168.0.1")
+    tvSn.Create_Sn_Tree()
+    tvSn.AddIfNot_Sn_account_Tree("192.168.0.11","luf@hotmail.ci")
+    g.add_ip("192.168.0.11")
+    g.ip("192.168.0.11").add_account("luf@hotmail.ci")
+    g.ip("192.168.0.11").account("luf@hotmail.ci").add_contact("lulu")
+    tvSn.AddIfNot_Sn_account_Tree("192.168.0.10","luf@hotmail.ci")
+    tvSn.Update_Sn_Tree()
+    tvSn.show()
+    #tvSn.set_label_speech(g.ip("192.168.0.3").account("piouf@hotmail.com").contact("nun").speech)
+    #tvSn.set_label_selected("192.168.0.3->piouf@hotmail.com->nun")
+    main()
+
